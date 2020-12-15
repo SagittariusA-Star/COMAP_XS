9860c0ace80c58d3dd94225aa7fab630db399536
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2020-12-15 22:11:32 +0100

    Add files via upload

--- read_multisplit.py
+++ read_multisplit.py
@@ -51,11 +51,11 @@ def read_map(mappath,field, control_variables, test_variables, feed_feed_variabl
    multisplits = input_map['multisplits']
    maps_created = []
    for ff_variable in feed_feed_variables:
-      print (ff_variable)
+      print ('ff',ff_variable)
       for test_variable in test_variables:
-         print (test_variable)
+         print ('test',test_variable)
          if test_variable != ff_variable: 
-            print (ff_variable, test_variable)
+            print ('fit',ff_variable, test_variable)
             map_split = np.array(multisplits['map_' + test_variable][:])
             rms_split = np.array(multisplits['rms_' + test_variable][:])
             shp = map_split.shape
