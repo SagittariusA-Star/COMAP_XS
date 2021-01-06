321228d86a0c9957d396eceb2e05e5ea86343f06
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2021-01-06 13:09:46 +0100

    Add files via upload

--- additional_methods.py
+++ additional_methods.py
@@ -110,7 +110,7 @@ def xs_with_model_3fields(figure_name, k, xs_mean2, xs_mean6, xs_mean7, xs_sigma
    ax1.plot(k, 0 * xs_mean2, 'k', alpha=0.4, zorder=1)
    #ax1.plot(k, k*PS_function.PS_f(k)/ transfer(k), label='k*PS of the input signal')
    #ax1.plot(k, k*PS_function.PS_f(k), label='k*PS of the input signal')
-   ax1.plot(k_th, k_th * ps_th_nobeam * 5, '--', label=r'$5 \times kP_{Theory}(k)$', color='dodgerblue')
+   ax1.plot(k_th, k_th * ps_th_nobeam * 10, '--', label=r'$10 \times kP_{Theory}(k)$', color='dodgerblue')
    #ax1.plot(k_th, k_th * ps_copps_nobeam * 5, 'g--', label=r'$5 \times kP_{COPPS}$ (shot)')
    ax1.set_ylabel(r'$k\tilde{C}(k)$ [$\mu$K${}^2$ Mpc${}^2$]', fontsize=14)
    if scan_strategy == 'ces':
