89b081fd903d0e60251f9cc82ddc468900f7e6ce
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2021-01-11 21:42:18 +0100

    Add files via upload

--- mean_multisplit.py
+++ mean_multisplit.py
@@ -264,7 +264,7 @@ def xs_feed_feed_2D(map_file):
       noise = np.zeros_like(chi2)
       for i in range(n_feed): #go through all the feed combinations
          for j in range(n_feed):
-            if i != 7 and j != 7:
+            #if i != 7 and j != 7:
               try:
                   filepath = path_to_xs %(i+1, j+1)
                   with h5py.File(filepath, mode="r") as my_file:
