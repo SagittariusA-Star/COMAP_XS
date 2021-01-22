97065f139c2540e4f078f8112456c60079261c17
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2021-01-22 15:57:22 +0100

    Add files via upload

--- additional_methods.py
+++ additional_methods.py
@@ -343,7 +343,7 @@ def plot_nulltest(cesc):
    plt.tight_layout()
    if cesc == '0':
       plt.savefig('nulltests_3fields_liss.pdf')
-    if cesc == '1':
+   if cesc == '1':
       plt.savefig('nulltests_3fields_ces.pdf')
 
 plot_nulltest('0')
