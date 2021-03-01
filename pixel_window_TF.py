4fda303594c8d5ac8d3e9a585c92d99877f9c144
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2021-03-01 23:32:41 +0100

    Add files via upload

--- pixel_window_TF.py
+++ pixel_window_TF.py
@@ -1,14 +1,18 @@
 import numpy as np
 import scipy.interpolate
+import matplotlib.pyplot as plt
 
 def pw():
    ps_low_res = np.load('ps_low_res.npy')
    ps_high_res = np.load('ps_high_res.npy')
    k = np.load('k_arr.npy')
+   pw_tf = np.load('pixel_window.npy')
    k = k[0]
 
-   pixel_window = ps_low_res[0]/ps_high_res[0]
+   #pixel_window = ps_low_res[0]/ps_high_res[0]
+   pixel_window = pw_tf
    transfer_pixel_window = scipy.interpolate.interp2d(k[0], k[1], pixel_window)
+  
    return transfer_pixel_window
 
 
