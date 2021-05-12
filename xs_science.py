b8c5ebf35e5bb6cc7c1da6ebe1f15c0567e560a6
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2021-05-12 12:25:20 +0200

    Add files via upload

--- xs_science.py
+++ xs_science.py
@@ -171,7 +171,12 @@ plot_nulltest('1')
 
 
 #now plot one of these in 2D
-#def xs_2D_plot_null(figure_name, k,k_bin_edges_par, k_bin_edges_perp, xs_mean1,xs_mean2,xs_mean3,xs_mean4, xs_mean5,xs_mean6, titlename, test1, test2, test3, field):
+
+def log2lin(x, k_edges):
+    loglen = np.log10(k_edges[-1]) - np.log10(k_edges[0])
+    logx = np.log10(x) - np.log10(k_edges[0])
+    return logx / loglen
+
 def xs_2D_plot_null(index_liss, index_ces, figure_name):
       fields = ['co2', 'co6', 'co7']
       k_c2, xs_mean_c2, xs_sigma_c2, k_edges_perp_c2, k_edges_par_c2 = read_h5_arrays(fields[0] + '_map_null_2D_arrays.h5', two_dim=True)
@@ -213,7 +218,7 @@ def xs_2D_plot_null(index_liss, index_ces, figure_name):
       img5 = ax[1][1].imshow(xs_mean5/TF_CES_2D(k[0],k[1]), interpolation='none', origin='lower',extent=[0,1,0,1], cmap='magma', norm=norm)
       fig.colorbar(img5, ax=ax[1][1], fraction=0.046, pad=0.13, orientation='horizontal')
       img6 = ax[1][2].imshow(xs_mean6/TF_CES_2D(k[0],k[1]), interpolation='none', origin='lower',extent=[0,1,0,1], cmap='magma', norm=norm)
-      fig.colorbar(img6, ax=ax[1][2], fraction=0.046, pad=0.13, orientation='horizontal').set_label(r'$\tilde{C}^{\mathrm{CES}}_{k_{\bot}, k_{\parallel}}$', size=18)
+      fig.colorbar(img6, ax=ax[1][2], fraction=0.046, pad=0.13, orientation='horizontal').set_label(r'$\tilde{C}\left(k_{\bot},k_{\parallel}\right)$ [$\mu$K${}^2$ (Mpc)${}^3$], CES', size=18)
       
       
      
