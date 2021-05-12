6b9eacbe1723bb6d0b28bcdfd979d75b1da76da6
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2021-05-12 13:56:50 +0200

    Add files via upload

--- fetch_chi2.py
+++ fetch_chi2.py
@@ -78,9 +78,8 @@ def xs_feed_feed_grid(map_file, figure_name):
               noise[i,j] = 1 / np.sqrt(w)
               chi3 = np.sum((xs[i,j] / rms_xs_std[i,j]) ** 3) #we need chi3 to take the sign into account - positive or negative correlation
 
-              chi2[i, j] = np.sign(chi3) * abs((np.sum((xs[i,j] / rms_xs_std[i,j]) ** 2) - n_k) / np.sqrt(2 * n_k)) #magnitude (how far from white noise)
-            
-              
+              chi2[i, j] = np.sign(chi3) * abs((np.sum((xs[i,j] / rms_xs_std[i,j]) ** 2) - n_k) / np.sqrt(2 * n_k))      
+              print (abs(chi2[i,j]))
               #if abs(chi2[i,j]) < 5. and not np.isnan(chi2[i,j]) and i != j:  #if excess power is smaller than 5 sigma, chi2 is not nan, not on diagonal
               if not np.isnan(chi2[i,j]) and i != j:
                   went_through_first_cut += 1
