bfcc9e273cf9f95a603f26a9c38dddb9df4e4fe7
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2021-05-13 17:54:52 +0200

    Add files via upload

--- xs_science.py
+++ xs_science.py
@@ -519,7 +519,7 @@ def xs_1D_3fields(figure_name, scan_strategy, index):
 
 
 def plot_combined_and_model(figure_name):
-   xs_data, sigma_data, k = coadd_all_ces()
+   xs_data, sigma_data, k = coadd_CO7()
    P_theory_new = np.load('ps_theory_new_1D.npy')
    P_theory_new = 1e-12*np.mean(P_theory_new, axis=0) #this factor accounts for the fact that I wrongly converted units in the simulated maps
    k_th = np.load('k.npy')
@@ -530,10 +530,10 @@ def plot_combined_and_model(figure_name):
    lim = np.mean(np.abs(xs_data[4:-2] * k[4:-2])) * 8
    fig, ax = plt.subplots(nrows=2,ncols=1,figsize=(10,6))
    ax[0].errorbar(k, k * xs_data, k * sigma_data, fmt='o', label=r'$k\tilde{C}(k)$, CES', color='black', zorder=4)
-   ax[0].plot(k_th, k_th * P_theory_old * 5, '--', label=r'$5kP_{Theory}(k)$', color='teal', zorder=3)
+   ax[0].plot(k_th, k_th * P_theory_old * 10, '--', label=r'$10kP_{Theory}(k)$', color='teal', zorder=3)
    #ax.plot(k_th, P_theory_old, '--', label=r'$\times P_{Theory, old}(k)$', color='dodgerblue')
-   ax[0].plot(k, k * P_theory_new  * 5, label=r'$5k\tilde{P}_{Theory, \parallel smooth}(k)$', color='purple') #smoothed in z-direction
-   ax[0].plot(k, k * P_notsmooth  * 5, label=r'$5k\tilde{P}_{Theory}(k)$', color='salmon') #not smoothed
+   ax[0].plot(k, k * P_theory_new  * 10, label=r'$10k\tilde{P}_{Theory, \parallel smooth}(k)$', color='purple') #smoothed in z-direction
+   ax[0].plot(k, k * P_notsmooth  * 10, label=r'$10k\tilde{P}_{Theory}(k)$', color='salmon') #not smoothed
    #ax.set_ylim(-lim*3, lim*3) 
    ax[0].set_ylim(-30000, 30000) 
    ax[0].plot(k, 0 * xs_data, 'k', alpha=0.4, zorder=1)
