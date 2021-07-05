f2a3bf5bfc55ad761d2220fbeecde34b16a7b97b
Jowita Borowska <54434838+jowborowska@users.noreply.github.com>
2021-07-05 14:24:36 +0200

    Add files via upload

--- transfer_functions.py
+++ transfer_functions.py
@@ -606,7 +606,7 @@ def total_2D_plot_whole(figure_name,liss, ces,mix,k_perp, k_par):
     
       img1 = ax[0].imshow(liss(k_perp, k_par), interpolation='none', origin='lower',extent=[0,1,0,1], cmap='magma', norm=norm_tf)
       #fig.colorbar(img1, orientation='horizontal', ax=ax[0],fraction=0.046, pad=0.13).set_label(r'$\tilde{T}^{\mathrm{total}}_{k_{\bot}, k_{\parallel}}$, Lissajous', size=18)
-      fig.colorbar(img1, orientation='horizontal', ax=ax[0],fraction=0.046, pad=0.13).set_label(r'$\tilde{T}^{\mathrm{total}}_{k_{\bot}, k_{\parallel}}$', size=18)
+      fig.colorbar(img1, orientation='horizontal', ax=ax[0],fraction=0.046, pad=0.13).set_label(r'$\tilde{T}^{\mathrm{total}}_{k_{\bot}, k_{\parallel}}$, Lissajous and CES', size=18)
 
       img2 = ax[1].imshow(ces(k_perp, k_par), interpolation='none', origin='lower',extent=[0,1,0,1], cmap='magma', norm=norm_tf)
       fig.colorbar(img2,orientation='horizontal', ax=ax[1], fraction=0.046, pad=0.13).set_label(r'$\tilde{T}^{\mathrm{total}}_{k_{\bot}, k_{\parallel}}$, CES', size=18)
